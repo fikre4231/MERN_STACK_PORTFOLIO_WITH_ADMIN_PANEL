5f2f77bfcc2fca8ecab85f8f80e66d1d62b29800
fikre4231 <fikrebereda@gmail.com>
2024-12-03 03:07:53 -0800

    yitbarek

--- dashboard/src/pages/HomePage.jsx
+++ dashboard/src/pages/HomePage.jsx
@@ -55,7 +55,7 @@ const HomePage = () => {
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
             <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-            <span className="sr-only">Acme Inc</span>
+            <span className="sr-only">Yitbarek Inc</span>
           </Link>
           <TooltipProvider>
             <Tooltip>
